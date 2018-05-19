0c19358972f26c43f7d56c9b401482ab77f1667f
Daniel Szabo <dgszabo@gmail.com>
2018-05-19 14:35:12 -0700

    Page swapping logic designed and tested on one page

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,27 @@
 $(document).ready(() => {
-    setTimeout(() => {
-        $(".load-cover").fadeOut();
-    }, 2000)
+    // setTimeout(() => {
+    //     $(".load-cover").fadeOut();
+    // }, 2000)
 
-    
+    $('.load-cover').fadeOut();
+    $('#home-btn').click((event) => {
+        event.preventDefault();
+        console.log('you pushed the button')
+        $('.home-page').toggleClass('left-screen')
+        setTimeout(() => {
+                $(".load-cover").fadeIn();
+            }, 2000)
+    });
+    $('#about-btn').click((event) => {
+        event.preventDefault();
+        console.log('you pushed the button')
+    })
+    $('#portfolio-btn').click((event) => {
+        event.preventDefault();
+        console.log('you pushed the button')
+    })
+    $('#contact-btn').click((event) => {
+        event.preventDefault();
+        console.log('you pushed the button')
+    })
 });
